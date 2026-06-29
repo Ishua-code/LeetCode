415ccfe8e4f5a6a7f21273f2eef8b83db7694028
Ishua-code <ishua2419@gmail.com>
2026-06-29 22:41:14 +0530

    Added Binary Tree

---
+++ Binary_Tree/145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,41 @@
+"""145. Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def dfs(root,res):
+            if not root:
+                return None
+            dfs(root.left,res)
+            dfs(root.right,res)
+            res.append(root.val)
+        dfs(root,res)
+        return res
+
+ 
